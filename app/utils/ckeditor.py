36c22ad9bbaa3e4867a1f233c468cb89418c0a4e
flappeh <bonar_david@rocketmail.com>
2024-07-31 15:55:59 +0700

    fixed logger for requests

--- app/utils/ckeditor.py
+++ app/utils/ckeditor.py
@@ -3,6 +3,9 @@ import requests as req
 from utils.utils import generateSignature
 from time import time
 from utils.exceptions import DocumentNotFoundError
+from utils.utils import get_logger
+
+logger = get_logger()
 
 def generate_headers(timestamp, signature):
     return{
@@ -11,6 +14,7 @@ def generate_headers(timestamp, signature):
     }
 
 def get_document_from_storage(id: str):
+    logger.info(f'Requesting document : {id} to document storage')
     url = CKEDITOR_COLLAB_API_URL + f'storage/{id}'
     timestamp = round(time() * 1000)
     signature = generateSignature(method='GET', timestamp=timestamp,uri=url,body=None)
@@ -19,10 +23,12 @@ def get_document_from_storage(id: str):
         res.raise_for_status()
         return res.text
     except req.RequestException as e:
+        logger.error(f'Failed requesting document : {id}, message {e.response}')
         raise DocumentNotFoundError(id)
     
 
 def get_suggestions_from_documentId(id: str):
+    logger.info(f'Requesting suggestion for document : {id}')
     url = CKEDITOR_COLLAB_API_URL + f'suggestions?document_id={id}&limit=1000&sort_by=updated_at&order=desc'
     timestamp = round(time() * 1000)
     signature = generateSignature(method='GET', timestamp=timestamp,uri=url,body=None)
@@ -31,6 +37,7 @@ def get_suggestions_from_documentId(id: str):
         res.raise_for_status()
         return res.json()
     except req.RequestException as e:
+        logger.error(f'Failed requesting document suggestions for documentId : {id}, message {e.response}')
         raise DocumentNotFoundError(id)
     
     
